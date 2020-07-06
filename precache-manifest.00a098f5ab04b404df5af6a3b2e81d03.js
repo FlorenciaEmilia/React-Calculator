1c8176961a2ecac120c903010debd2adeb10e08f
Florencia <flxrenciaemilia@gmail.com>
2020-07-06 14:02:20 -0500

    Updates

---
+++ precache-manifest.00a098f5ab04b404df5af6a3b2e81d03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219950b9efc40a0593ffa25c3a7c2798",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "7a958dbfe345f5bac484",
+    "url": "/React-Calculator/static/css/main.17a90b5d.chunk.css"
+  },
+  {
+    "revision": "5ffba4bd51ac7ec185e9",
+    "url": "/React-Calculator/static/js/2.e1f64976.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/React-Calculator/static/js/2.e1f64976.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a958dbfe345f5bac484",
+    "url": "/React-Calculator/static/js/main.b02c0522.chunk.js"
+  },
+  {
+    "revision": "3cf58007e6f679e36a58",
+    "url": "/React-Calculator/static/js/runtime-main.77f7a837.js"
+  }
+]);
